9b1f613ed92c9d956b1474b38c1cce0c900cfd9b
Gonzalo Guerrero <elias.gonzaloguerrero@gmail.com>
2022-11-28 23:58:20 -0300

    props & task(title, id, description) added at App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import TaskList from './TaskList'
-import TaskForm from './TaskForm'
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
-function HolaMundo() {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle) {
+    setTasks([...tasks, {
+      title: taskTitle,
+      id: tasks.length,
+      description: "nueva tarea"
+    }])
+  }
+
   return (
     <>
-      <TaskForm/>
-      <TaskList/>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} />
     </>
-  )
+  );
 }
 
-export default HolaMundo
+export default App;
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,15 +1,7 @@
-import { tasks as data } from "./tasks";
-import { useState, useEffect } from "react";
-
-function TaskList() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
+function TaskList({tasks}) {
+  
   if (tasks.length === 0) {
-    return <h1>No hay tareas aun</h1>;
+    return <h1>No hay tareas aun</h1>
   }
 
   return (
@@ -17,7 +9,7 @@ function TaskList() {
       {tasks.map((tasks) => (
         <div key={tasks.id}>
           <h1>{tasks.title}</h1>
-          <p>{tasks.desscription}</p>
+          <p>{tasks.description}</p>
         </div>
       ))}
     </div>
