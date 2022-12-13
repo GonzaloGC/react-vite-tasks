0e04a119ce37cd2ac5c35b12dc5a2d3a285ba86b
Gonzalo Guerrero <elias.gonzaloguerrero@gmail.com>
2022-12-12 23:58:16 -0300

    added TaskContext

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,19 @@
 import TaskCard from './TaskCard'
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
-function TaskList({tasks, deleteTask}) {
+function TaskList() {
+  const { tasks } = useContext(TaskContext);
   
   if (tasks.length === 0) {
-    return <h1>No hay tareas aun</h1>
+    return (<h1>No hay tareas aun</h1>
+    );
   }
 
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} deleteTask={deleteTask}  />
+        <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
